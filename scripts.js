c5d400a0655b8d9844a6015035c191a4a64e55ec
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-01-18 13:46:13 +0600

    wrote the formula pressure

---
+++ scripts.js
@@ -0,0 +1,10 @@
+let m= +prompt("What is M");
+let v= +prompt("What is V");
+let sum= m * v;
+
+let t= +prompt("What is T");
+let N= +prompt("What is N");
+let sum= t * N;
+
+console.log(`M mutliplied by V is egual to ?${sum}`);
+console.log(`T mutliplied by N is egual to ?${sum}`);
